45a0b5d066c88736f25e69bbad0cac267320cf73
Tuss4 <tuss4dbfn@gmail.com>
2015-06-25 00:36:37 -0500

    Update client.go to keep a persistent connection to the server.

--- client.go
+++ client.go
@@ -9,28 +9,34 @@ import (
 )
 
 func main() {
-	var msg string
 	var uname string
-	print("Enter your name: ")
-	fmt.Scanln(&uname)
-	print("Enter your message: ")
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	msg = scanner.Text()
 	conn, err := net.Dial("tcp", "localhost:5050")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer conn.Close()
-	message := []byte(msg)
-	_, err = conn.Write(message)
-	if err != nil {
-		log.Fatal(err)
+	println("Connection established.")
+	print("Enter your name: ")
+	fmt.Scanln(&uname)
+	// Get a loop going
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		switch {
+		case scanner.Text() == "quit":
+			conn.Close()
+			os.Exit(1)
+		default:
+			message := []byte(scanner.Text())
+			_, err = conn.Write(message)
+			if err != nil {
+				log.Fatal(err)
+			}
+			rb := make([]byte, 1024)
+			resp, err := conn.Read(rb)
+			if err != nil {
+				log.Fatal(err)
+			}
+			msg_strt := fmt.Sprintf("%v:", uname)
+			println(msg_strt, string(rb[:resp]))
+		}
 	}
-	rb := make([]byte, 1024)
-	resp, err := conn.Read(rb)
-	if err != nil {
-		log.Fatal(err)
-	}
-	println(uname, ":", string(rb[:resp]))
 }
